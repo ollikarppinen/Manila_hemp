af86477c437e488b553320b81acc11ad50aac71b
ollikarppinen <ollikarppinen@gmail.com>
2015-12-11 21:45:46 +0200

    Refactored code to conform PEP8 style guide.

--- ManilaHemp.py
+++ ManilaHemp.py
@@ -1,114 +1,140 @@
-import queue, threading, json
+import queue
+import threading
+import json
 from urllib.request import urlopen
 from urllib.parse import quote
 
+
+# Wikipedia article
+# @title: Title of the Wiki article
+# @father: The article which had a link to this one
+# @reverse: True if this article originates from start article
+#          False if this article originates from goal article
 class article:
-	def __init__(self, title, father):
-		self.title = title
-		self.father = father
 
-def apiCall(url, title):
-	response = urlopen(url + quote(title))
-	str_response = response.readall().decode('utf-8')
-	obj = json.loads(str_response)
-	return obj
+    def __init__(self, title, father, reverse):
+        self.title = title
+        self.father = father
+        self.reverse = reverse
 
+
+# Makes Wikipedia API call
+# @url: API calls URL as String
+# @title: Title of the Wiki article that is being used in API call
+# returns: JSON object
+def apiCall(query, title):
+    url = 'http://en.wikipedia.org/w/api.php?'
+    response = urlopen(url + query + quote(title))
+    str_response = response.read().decode('utf-8')
+    obj = json.loads(str_response)
+    obj_pages = obj['query']['pages']
+    obj_links = list(map(lambda x: obj_pages[x], obj_pages.keys()))
+    return obj_links[0]
+
+
+# /w/api.php?action=query&prop=linkshere&format=json&lhnamespace=0&lhlimit=500&titles=
 def linksHere(title):
-	obj = apiCall('http://en.wikipedia.org/w/api.php?action=query&prop=linkshere&format=json&lhprop=pageid%7Ctitle&lhlimit=5000&titles=', title)
-	arr = []
-	for i in obj['query']['pages']:
-		if 'linkshere' in obj['query']['pages'][i].keys():
-			for j in obj['query']['pages'][i]['linkshere']:
-				if j['ns'] == 0:
-					arr += [j['title']]
-	return arr
+    obj = apiCall(
+        '&action=query'
+        '&prop=linkshere'
+        '&format=json'
+        '&lhnamespace=0'
+        '&lhlimit=500'
+        '&titles=', title)
+    try:
+        return list(map(lambda x: x['title'], obj['linkshere']))
+    except KeyError:
+        print("KeyError")
+
 
 def links(title):
-	obj = apiCall('http://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&pllimit=5000&titles=', title)
-	arr = []
-	for i in obj['query']['pages']:
-		if 'links' in obj['query']['pages'][i].keys():
-			for j in obj['query']['pages'][i]['links']:
-				if j['ns'] == 0:
-					arr += [j['title']]
-	return arr
+    obj = apiCall(
+        'action=query'
+        '&prop=links'
+        '&format=json'
+        '&plnamespace=0'
+        '&pllimit=500'
+        '&titles=', title)
+    try:
+        return list(map(lambda x: x['title'], obj['links']))
+    except KeyError:
+        print("KeyError")
 
-def path(goal, start, article):
-	a = article
-	str = goal
-	while not a.title == start:
-		str = a.title + " -> " + str
-		a = a.father
-	return start + " -> " + str
 
-#Basic settings and variables
-#start = "Army_Group_Centre"
-#goal = "Adolf_Hitler"
-#start = "Heat_(1996_film)"
+def path(article, link):
+    a = article
+    str = goal
+    while not a.title == start:
+        str = a.title + " -> " + str
+        a = a.father
+    return start + " -> " + str
+
+
+'''
+Basic settings and variables
+start = "Army_Group_Centre"
+goal = "Adolf_Hitler"
+start = "Heat_(1996_film)"
+'''
+
 start = input("Start article: ")
 goal = input("Goal article: ")
 
 num_worker_threads = 5
+
+# Job queues for workers
+jobs = queue.Queue()
+r_jobs = queue.Queue()
+
+# Article arrays to keep track of the link paths
+articles = []
+r_articles = []
+
+# Arrays for checking if path has been found
+titles = []
+r_titles = []
+
+# Flag: True if path has been found
+f = False
+
+num_worker_threads = 50
 q = queue.Queue()
 visited = []
-goals = linksHere(goal)
+goalTitles = linksHere(goal)
 f = True
 
-#Worker thread
+# Worker thread
+
+
 def worker():
-	global start
-	global goal
-	global f
-	global visited
-	while True:
-		if not f:
-			q.task_done()
-		elif q:
-			a = q.get()
-			obj = links(a.title)
-			for i in obj:
-				if not i in visited:
-					newArticle = article(i, a)
-					if i in goals:
-						f = False
-						print(path(goal, start, newArticle))
-					else:
-						visited += i
-						q.put(newArticle)
-	
+    global start
+    global goal
+    global f
+    global visited
+    while f:
+        a = q.get()
+        obj = links(a.title)
+        if obj:
+            for i in obj:
+                if not i in visited:
+                    newArticle = article(i, a, False)
+                    if i in goalTitles:
+                        f = False
+                        print(path(newArticle, i))
+                        break
+                    else:
+                        visited += i
+                        q.put(newArticle)
+                if not f:
+                    break
+        q.task_done()
 
-#Start worker threads
+
+# Start worker threads
 for i in range(num_worker_threads):
-	t = threading.Thread(target=worker)
-	t.daemon = True
-	t.start()
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
 
-q.put(article(start, ""))
+q.put(article(start, "", False))
 q.join()
-
-'''
-import Queue, threading
-
-#Basic settings and variables
-num_worker_threads = 5
-q = Queue.Queue()
-
-#Worker thread
-def worker():
-	while True:
-			item = q.get()
-			print item
-			q.task_done()
-
-#Start worker threads
-for i in range(num_worker_threads):
-	t = threading.Thread(target=worker)
-	t.daemon = True
-	t.start()
-
-
-for i in range(10):
-	q.put(i)
-
-q.join()
-'''
